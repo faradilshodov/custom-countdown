8e32337d653cf415874db0e9b7a36a12b2b2fe77
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-04-25 13:27:02 -0400

    add grabbing form values

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const inputContainer = document.getElementById('input-container');
 const countdownForm = document.getElementById('countdown-form');
 const dateEl = document.getElementById('date-picker');
 
+let countdownTitle = '';
+let countdownDate = '';
+
 // Set Date Input Min with Today's Date
 const today = new Date().toISOString().split('T')[0];
 dateEl.setAttribute('min', today);
+
+// Take Valuse from Form Input
+function updateCountdown(e) {
+    e.preventDefault();
+    countdownTitle = e.srcElement[0].value;
+    countdownDate = e.srcElement[1].value;
+}
+
+// Event Listeners
+countdownForm.addEventListener('submit', updateCountdown);
